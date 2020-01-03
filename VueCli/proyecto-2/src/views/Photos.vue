ac086774e2c807e7f89b058255a9704475f7c577
Angelo Castillo <angcastillo18@gmail.com>
2020-01-02 21:46:22 -0500

    add proyect 3 with bootstrap

--- VueCli/proyecto-2/src/views/Photos.vue
+++ VueCli/proyecto-2/src/views/Photos.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
       <h1>Galeria de fotos</h1>
+      <!-- rutas dinamicas  -->
+      <router-link :to="{name:'photos',params:{id:item}}" 
+      v-for="(item, index) in fotosArray" :key="index">
+          <button>foto {{item}}</button>
+      </router-link>
       <Photo/>
+    <!-- Escrubiendo la ruta descrita en los methods-->
+      <button @click="home">Home</button>
+      <button @click="anterior">Anterior</button>
+      <button @click="siguiente">Siguiente</button>
   </div>
 </template>
 
@@ -12,6 +21,24 @@ import Photo from "@/components/Photo.vue"
 export default {
     components:{
         Photo
+    },
+    data(){
+        return{
+            fotosArray: [1,2,3]
+        }
+    },
+    methods:{
+        /* Ir a la ruta con path / */
+        home(){
+            this.$router.push('/');
+        },
+        /* Ir en el historial atras */
+        anterior(){
+            this.$router.go(-1);
+        },
+        siguiente(){
+            this.$router.go(1);
+        }
     }
 }
 </script>
